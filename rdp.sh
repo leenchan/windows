40a3a04b222802b766f1e6d9e1c2d15bea51f94c
leenchan <leenchan@gmail.com>
2021-10-11 16:14:37 +0800

    Create rdp.sh

---
+++ rdp.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+frpc_conf() {
+	cat <<-EOF > frpc.ini
+	[common]
+	server_addr = frp2.freefrp.net
+	server_port = 7000
+	token = freefrp.net
+
+	[rdp_43389]
+	type = tcp
+	remote_port = 43389
+	use_encryption = true
+	use_compression = true
+	local_ip = 127.0.0.1
+	local_port = 3389
+	EOF
+}
+
+frpc_conf
