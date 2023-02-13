1ec581ec0a1dee79233841401f5beef6b5a8308d
leenchan <leenchan@gmail.com>
2023-02-13 23:16:13 +0800

    Create rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -5,7 +5,7 @@ RDP_PASSWD_DEFAULT="_Password_"
 FRP_SERVER_PORT=${FRP_SERVER_PORT:-7000}
 FRP_REMOTE_PORT=${FRP_REMOTE_PORT:-43389+}
 
-[ -z "$FRPC_SERVER_HOST" ] && {
+[ -z "$FRP_SERVER_HOST" ] && {
 	FRP_SERVER_HOST="frp3.freefrp.net"
 	FRP_SERVER_TOKEN="freefrp.net"
 }
