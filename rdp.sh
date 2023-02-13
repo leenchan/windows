af925c33c604367d0d4e2f9ac6b878954b4b544e
leenchan <leenchan@gmail.com>
2023-02-14 00:12:46 +0800

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -15,12 +15,18 @@ change_password() {
 	net user runneradmin "$RDP_PASSWD" || exit 1
 }
 
+frp_check_server() {
+	curl -skL "$FRP_SERVER_HOST:$FRP_REMOTE_PORT"
+	[ "$?" = "52" ] && return 0
+	return 1
+}
+
 frpc_get_port() {
-	_OK_=0
+	_REMOTE_PORT_=0
 	_OFFSET_=20
 	FRP_REMOTE_PORT=$(echo "$1" | tr -d '\-\+')
 	[ "$FRP_REMOTE_PORT" -ge 1 -a "$FRP_REMOTE_PORT" -le 65535 ] || return 1
-	echo "$1" | grep -Eq '[-+]$' || _OFFSET_=0
+	echo "$1" | grep -Eq '[-+]$' || (_OFFSET_=0; _REMOTE_PORT_="$FRP_REMOTE_PORT")
 	echo "$1" | grep -Eq '[-]$' && FRP_REMOTE_PORT=$((FRP_REMOTE_PORT-_OFFSET_))
 	while true
 	do
@@ -29,13 +35,13 @@ frpc_get_port() {
 		curl -skL "${FRP_SERVER_HOST}:${FRP_REMOTE_PORT}"
 		_CODE_="$?"
 		[ "$_CODE_" = "6" ] && break
-		[ "$_CODE_" = "7" -o "$_CODE_" = "56" ] && _OK_="1" && break
+		[ "$_CODE_" = "7" -o "$_CODE_" = "56" ] && _REMOTE_PORT_="$FRP_REMOTE_PORT" && break
 		FRP_REMOTE_PORT=$((FRP_REMOTE_PORT+1))
 		_OFFSET_=$((_OFFSET_-1))
 	done
-	[ "$_OK_" = "1" ] && echo "[OK] Set FRP remote port to: ${FRP_REMOTE_PORT}" && return 0
-	echo "[ERR] Faile to test frp server host / remote port."
-	return 1
+	[ "$_REMOTE_PORT_" = "0" ] && echo "[ERR] Faile to test frp server host / remote port." && return 1
+	echo "[OK] Set FRP remote port to: ${_REMOTE_PORT_}"
+	return 0
 }
 
 frpc_conf() {
